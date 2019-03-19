011d6300cd930ee912c20d86b8d781a0115d38a8
Michal Vlasák <michal.vlasak@ackee.cz>
2019-03-19 16:57:12 +0100

    Convert to camelCase only when plugin loaded

--- initBookshelf.js
+++ initBookshelf.js
@@ -3,6 +3,7 @@ const instances = new Map();
 module.exports = (knex, plugins = ['registry', 'bookshelf-camelcase', 'visibility', 'pagination', 'bookshelf-paranoia']) => {
     const bookshelf = require('bookshelf')(knex); // eslint-disable-line global-require
     bookshelf.__rdbGwKey = knex.__rdbGwKey;
+    bookshelf.__rdbGwCamelCase = plugins.includes('bookshelf-camelcase');
     plugins.forEach(plugin => {
         bookshelf.plugin(plugin);
     });
