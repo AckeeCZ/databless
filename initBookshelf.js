80bf68ef359fc522595669ad07c9bab1b8158229
Michal Vlasák <michal.vlasak@ackee.cz>
2019-07-02 15:42:43 +0200

    Add cursor pagination support

--- initBookshelf.js
+++ initBookshelf.js
@@ -1,9 +1,10 @@
 const instances = new Map();
 
-module.exports = (knex, plugins = ['registry', 'bookshelf-camelcase', 'visibility', 'pagination', 'bookshelf-paranoia']) => {
+module.exports = (knex, plugins = ['registry', 'bookshelf-camelcase', 'visibility', 'pagination', 'bookshelf-paranoia', 'bookshelf-cursor-pagination']) => {
     const bookshelf = require('bookshelf')(knex); // eslint-disable-line global-require
     bookshelf.__rdbGwKey = knex.__rdbGwKey;
     bookshelf.__rdbGwCamelCase = plugins.includes('bookshelf-camelcase');
+    bookshelf.__rdbgwCursorPagination = plugins.includes('bookshelf-cursor-pagination');
     plugins.forEach(plugin => {
         bookshelf.plugin(plugin);
     });
