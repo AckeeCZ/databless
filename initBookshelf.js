d17d54a3466efa7ac7a847993bf94958daebc668
Michal Vlasák <michal.vlasak@ackee.cz>
2019-07-03 14:15:09 +0200

    Better cursor plugin initialization

--- initBookshelf.js
+++ initBookshelf.js
@@ -1,15 +1,11 @@
 const instances = new Map();
+const cursorPlugin = require('bookshelf-cursor-pagination').default;
 
-module.exports = (knex, plugins = ['registry', 'bookshelf-camelcase', 'visibility', 'pagination', 'bookshelf-paranoia', 'bookshelf-cursor-pagination']) => {
+module.exports = (knex, plugins = ['registry', 'bookshelf-camelcase', 'visibility', 'pagination', 'bookshelf-paranoia', cursorPlugin]) => {
     const bookshelf = require('bookshelf')(knex); // eslint-disable-line global-require
     bookshelf.__rdbGwKey = knex.__rdbGwKey;
     bookshelf.__rdbGwCamelCase = plugins.includes('bookshelf-camelcase');
     bookshelf.__rdbgwCursorPagination = plugins.includes('bookshelf-cursor-pagination');
-    const cursorIndex = plugins.indexOf('bookshelf-cursor-pagination');
-    if (cursorIndex !== -1) {
-        plugins.splice(cursorIndex, 1);
-        plugins.push(require('bookshelf-cursor-pagination').default);
-    }
     plugins.forEach(plugin => {
         bookshelf.plugin(plugin);
     });
