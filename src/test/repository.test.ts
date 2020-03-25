ac1baf4893d320fb69e499b83d62439cd6b603b3
Jiří Šmolík <jiri.smolik@ackee.cz>
2020-03-25 17:16:14 +0100

    ✅ Add single model tests

--- src/test/repository.test.ts
+++ src/test/repository.test.ts
@@ -1,83 +1,245 @@
-import connect from 'knex';
+import { default as connect, default as Knex } from 'knex';
 import * as repository from '../lib/repository';
 
-describe('Repository (Knex)', () => {
-    const knex = connect({ client: 'sqlite3', connection: ':memory:', pool: { min: 1, max: 1 }, debug: false });
-    const whiskerModel = repository.createModel({
-        adapter: knex,
-        collectionName: 'whiskers',
-        attributes: {
-            length: { type: 'number' },
-            color: { type: 'string' },
-            cat_id: { type: 'number' },
-        },
-    });
-    const catModel = repository.createModel({
-        adapter: knex,
-        attributes: {
-            name: {
-                type: 'string',
-            },
-            mainWhisker: {
-                type: 'relation',
-                relation: repository.bookshelfRelation.createHasOne({
-                    foreignKey: 'cat_id',
-                }),
-                targetModel: () => whiskerModel,
-            },
-        },
-        collectionName: 'cats',
-    });
-    beforeAll(async () => {
-        // Make sure it works
-        await knex.raw('select 1');
-        // Run migrations
-        await knex.schema.createTable('cats', table => {
-            table.bigIncrements('id').primary();
-            table.string('name');
+const db = (() => {
+    let knex: ReturnType<typeof connect>;
+
+    /**
+     * Dummy migration runner for a model
+     * - PRIMARY KEY - attribute `id` is always set as BigInt Primary key
+     * @param model repository.Model
+     */
+    const createTable = async (model: repository.Model<any>) => {
+        // TODO Cast needed, model.options is any
+        const modelOptions: repository.ModelOptions = model.options;
+        await knex.schema.createTable(modelOptions.collectionName, table => {
+            Array.from(Object.entries(modelOptions.attributes)).forEach(([name, attribute]) => {
+                if (name === 'id') {
+                    table.bigIncrements(name).primary();
+                    return;
+                }
+                switch (attribute.type) {
+                    case 'string':
+                        table.string(name);
+                        break;
+                    case 'bool':
+                        table.boolean(name);
+                        break;
+                    case 'date':
+                        table.dateTime(name);
+                        break;
+                    case 'number':
+                        table.decimal(name);
+                        break;
+                    case 'object':
+                        table.jsonb(name);
+                        break;
+                    case 'relation':
+                        break;
+                    default:
+                        throw new TypeError('Invalid type');
+                }
+            });
         });
-        await knex.schema.createTable('whiskers', table => {
-            table.bigIncrements('id').primary();
-            table.decimal('length');
-            table.string('color');
-            table.bigInteger('cat_id').references('cats.id');
-        });
-    });
-    afterAll(async () => {
+    };
+    const reset = async () => {
+        if (knex) {
+            await knex.destroy();
+            knex = undefined as any;
+        }
+        knex = connect({ client: 'sqlite3', connection: ':memory:', pool: { min: 1, max: 1 }, debug: true });
+        return knex as Knex;
+    };
+    const disconnect = async () => {
         await knex.destroy();
+    };
+    return {
+        reset,
+        disconnect,
+        createTable,
+    };
+})();
+
+describe('Repository (Knex/Bookshelf)', () => {
+    afterAll(async () => {
+        await db.disconnect();
     });
-    test('Create', async () => {
-        const result = await repository.create(catModel, { name: 'Fluffy' });
-        expect(result).toMatchInlineSnapshot(`
-            Object {
-              "id": 1,
-              "name": "Fluffy",
-            }
-        `);
-    });
-    describe('Relation hasOne', () => {
-        let cat: any; // TODO Type
-        let whisker: any; // TODO Type
+    describe('Single model create', () => {
+        let model: repository.Model<any>; //TODO Type
         beforeAll(async () => {
-            cat = await repository.create(catModel, { name: 'Fluffy' });
-            whisker = await repository.create(whiskerModel, { length: 10, cat_id: cat.id });
+            const knex = await db.reset();
+            model = repository.createModel({
+                adapter: knex,
+                collectionName: 'model',
+                attributes: {
+                    id: { type: 'number' },
+                    string: { type: 'string' },
+                },
+            });
+            await db.createTable(model);
         });
-        it('Fetch using withRelated', async () => {
-            const populatedCat = await repository.detail(catModel, { id: cat.id }, { withRelated: ['mainWhisker'] });
-            expect(populatedCat).toMatchInlineSnapshot(`
-Array [
-  Object {
-    "id": 2,
-    "mainWhisker": Object {
-      "cat_id": 2,
-      "color": null,
-      "id": 1,
-      "length": 10,
-    },
-    "name": "Fluffy",
-  },
-]
-`);
+        test('Create with no data creates only automatically set fields', async () => {
+            const result = await repository.create(model, {});
+            expect(result).toMatchInlineSnapshot(`
+                Object {
+                  "id": 1,
+                  "string": null,
+                }
+            `);
+        });
+        test('Create sets given fields', async () => {
+            const result = await repository.create(model, { string: 'stringValue' });
+            expect(result).toMatchInlineSnapshot(`
+                Object {
+                  "id": 2,
+                  "string": "stringValue",
+                }
+            `);
+        });
+        test('Create ignores columns not set in attributes', async () => {
+            const result = await repository.create(model, { nonExisting: 'stringValue' });
+            expect(result).toMatchInlineSnapshot(`
+                Object {
+                  "id": 3,
+                  "string": null,
+                }
+            `);
         });
     });
+    describe('Single model read', () => {
+        let model: repository.Model<any>; //TODO Type
+        const inputData = [
+            {
+                string: 'abcdefg',
+                number: -10,
+            },
+            {
+                string: 'hijklmn',
+                number: 10,
+            },
+        ];
+        beforeAll(async () => {
+            const knex = await db.reset();
+            model = repository.createModel({
+                adapter: knex,
+                collectionName: 'model',
+                attributes: {
+                    id: { type: 'number' },
+                    string: { type: 'string' },
+                    number: { type: 'number' },
+                },
+            });
+            await db.createTable(model);
+            await Promise.all(
+                inputData.map(data => repository.create(model, data))
+            );
+        });
+        test('No params means no filtering', async () => {
+            const result = await repository.list(model);
+            expect(result.length).toEqual(inputData.length);
+        });
+        test('Exact match filtering', async () => {
+            const filter = { number: 10, string: 'hijklmn' };
+            const results = await repository.list(model, filter);
+            results.forEach(result => {
+                expect(result).toMatchObject(filter);
+            });
+        });
+        test('Count using filters', async () => {
+            const filter = { number: 10, string: 'hijklmn' };
+            const result = await repository.list(model, filter, { count: true });
+            expect(result).toEqual(
+                inputData
+                    .filter(data => (data.number === filter.number))
+                    .filter(data => (data.string === filter.string))
+                    .length
+            );
+        });
+    });
+    //     const ;
+    //     const reset = async () => {
+    //         if (knex) {
+    //             await knex.destroy();
+    //             knex = undefined as any;
+    //         }
+    //         knex = connect({ client: 'sqlite3', connection: ':memory:', pool: { min: 1, max: 1 }, debug: false });
+    //     };
+
+    //     const whiskerModel = repository.createModel({
+    //         adapter: knex,
+    //         collectionName: 'whiskers',
+    //         attributes: {
+    //             length: { type: 'number' },
+    //             color: { type: 'string' },
+    //             cat_id: { type: 'number' },
+    //         },
+    //     });
+    //     const catModel = repository.createModel({
+    //         adapter: knex,
+    //         attributes: {
+    //             name: {
+    //                 type: 'string',
+    //             },
+    //             mainWhisker: {
+    //                 type: 'relation',
+    //                 relation: repository.bookshelfRelation.createHasOne({
+    //                     foreignKey: 'cat_id',
+    //                 }),
+    //                 targetModel: () => whiskerModel,
+    //             },
+    //         },
+    //         collectionName: 'cats',
+    //     });
+    //     beforeAll(async () => {
+    //         // Make sure it works
+    //         await knex.raw('select 1');
+    //         // Run migrations
+    //         await knex.schema.createTable('cats', table => {
+    //             table.bigIncrements('id').primary();
+    //             table.string('name');
+    //         });
+    //         await knex.schema.createTable('whiskers', table => {
+    //             table.bigIncrements('id').primary();
+    //             table.decimal('length');
+    //             table.string('color');
+    //             table.bigInteger('cat_id').references('cats.id');
+    //         });
+    //     });
+    //     afterAll(async () => {
+    //         await knex.destroy();
+    //     });
+    //     test('Create', async () => {
+    //         const result = await repository.create(catModel, { name: 'Fluffy' });
+    //         expect(result).toMatchInlineSnapshot(`
+    //             Object {
+    //               "id": 1,
+    //               "name": "Fluffy",
+    //             }
+    //         `);
+    //     });
+    //     describe('Relation hasOne', () => {
+    //         let cat: any; // TODO Type
+    //         let whisker: any; // TODO Type
+    //         beforeAll(async () => {
+    //             cat = await repository.create(catModel, { name: 'Fluffy' });
+    //             whisker = await repository.create(whiskerModel, { length: 10, cat_id: cat.id });
+    //         });
+    //         it('Fetch using withRelated', async () => {
+    //             const populatedCat = await repository.detail(catModel, { id: cat.id }, { withRelated: ['mainWhisker'] });
+    //             expect(populatedCat).toMatchInlineSnapshot(`
+    // Array [
+    //   Object {
+    //     "id": 2,
+    //     "mainWhisker": Object {
+    //       "cat_id": 2,
+    //       "color": null,
+    //       "id": 1,
+    //       "length": 10,
+    //     },
+    //     "name": "Fluffy",
+    //   },
+    // ]
+    // `);
+    //         });
+    //     });
 });
