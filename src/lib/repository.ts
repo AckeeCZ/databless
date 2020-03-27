2a22b6f5eb1b594c15da12eb561a06e234c30601
Jaroslav Šmolík <grissius@gmail.com>
2020-03-27 14:23:45 +0100

    🏷️ Add options and filter interface

--- src/lib/repository.ts
+++ src/lib/repository.ts
@@ -1,6 +1,7 @@
 import Knex from 'knex';
 import { isEmpty, memoize, pick } from 'lodash';
 import * as bookshelfUtil from './bookshelfUtil';
+import { SerializeOptions } from 'bookshelf';
 
 type Primitive = 'string' | 'number' | 'date' | 'bool' | 'object';
 type PrimitiveToType<P> = P extends 'string' ? string : P extends 'date' ? Date : P extends 'number' ? number : P extends 'bool' ? boolean : P extends 'object' ? any : never;
@@ -30,6 +31,7 @@ export type AttributeRelation<A extends Record<string, PrimitiveAttribute> = Rec
 }
 export type Attributes2Entity<A extends Record<string, Attribute>> = { [key in keyof A]: A[key] extends Attribute ? Attribute2Type<A[key], A> : never };
 export type Attributes2RelationKeys<A extends Record<string, Attribute>> = { [key in keyof A]: A[key] extends { type: 'relation' } ? key : never }[keyof A];
+export type Attributes2NonRelationKeys<A extends Record<string, Attribute>> = { [key in keyof A]: A[key] extends { type: 'relation' } ? never : key }[keyof A];
 export type Model2Entity<M extends Model<any>> = Attributes2Entity<M['options']['attributes']>;
 export type Model2RelationKeys<M extends Model<any>> = Attributes2RelationKeys<M['options']['attributes']>;
 
@@ -49,17 +51,31 @@ export interface Model<A extends Record<string, Attribute> = Record<string, Attr
 
 export const bookshelfRelation = bookshelfUtil.bookshelfRelation;
 
-export const create = async <A extends Record<string, Attribute>>(model: Model<A>, data: Partial<Model2Entity<Model<A>>>, options?: any /* TODO Type */): Promise<Attributes2Entity<A>> => {
+interface RepositoryMethodOptions {
+    toJSON?: SerializeOptions;
+}
+interface RepositoryDetailOptions<A extends Record<string, Attribute>> extends RepositoryMethodOptions {
+    // TODO: This type restricts using withRelated on transitive fields
+    withRelated?: Attributes2RelationKeys<A>[];
+}
+interface RepositoryListOptions<A extends Record<string, Attribute>> extends RepositoryDetailOptions<A> {
+    count?: true;
+    order?: Attributes2NonRelationKeys<A> | Attributes2NonRelationKeys<A>[]
+}
+
+type Filters<A extends Record<string, Attribute>> = Partial<{[key in keyof A]: Attribute2Type<A[key], A> | Attribute2Type<A[key], A>[]}>;
+
+export const create = async <A extends Record<string, Attribute>>(model: Model<A>, data: Partial<Model2Entity<Model<A>>>, options?: RepositoryMethodOptions): Promise<Attributes2Entity<A>> => {
     data = model.serialize(data);
     const result = await (model.getBookshelfModel().forge())
         .save(pick(data, model.attributeNames), options);
     return bookshelfUtil.serializer(options)(result);
 };
 
-export const list = async <A extends Record<string, Attribute>>(model: Model<A>, filter?: Partial<Model2Entity<Model<A>>>, options?: any): Promise<Attributes2Entity<A>[]> => {
+export const list = async <A extends Record<string, Attribute>>(model: Model<A>, filter?: Filters<A>, options?: RepositoryListOptions<A>): Promise<Attributes2Entity<A>[]> => {
     const result = await bookshelfUtil.queryModel(model.getBookshelfModel(), filter, options)
         .fetchAll(options);
-    if (options && options.count) {
+    if (options?.count) {
         return (bookshelfUtil.serializer(options)(result));
     }
     return (bookshelfUtil.serializer(options)(result))
@@ -67,7 +83,7 @@ export const list = async <A extends Record<string, Attribute>>(model: Model<A>,
 };
 
 // TODO Options should have properties for current adapter, e.g. withRelated for Bookshelf. How?
-export const detail = async <A extends Record<string, Attribute>>(model: Model<A>, filter?: Partial<Model2Entity<Model<A>>>, options?: any): Promise<Attributes2Entity<A>> => {
+export const detail = async <A extends Record<string, Attribute>>(model: Model<A>, filter?: Filters<A>, options?: RepositoryDetailOptions<A>): Promise<Attributes2Entity<A>> => {
     // TODO DB Limit 1
     const result = await bookshelfUtil.queryModel(model.getBookshelfModel(), filter, options)
         .fetch(options);
@@ -81,7 +97,7 @@ export const detail = async <A extends Record<string, Attribute>>(model: Model<A
  * @param data 
  * @param options 
  */
-export const update = async <A extends Record<string, Attribute>>(model: Model<A>, filter: Partial<Model2Entity<Model<A>>>, data?: Partial<Model2Entity<Model<A>>>, options: any /* TODO Type*/ = {}): Promise<Attributes2Entity<A> | undefined> => {
+export const update = async <A extends Record<string, Attribute>>(model: Model<A>, filter: Partial<Model2Entity<Model<A>>>, data?: Partial<Model2Entity<Model<A>>>, options: RepositoryMethodOptions = {}): Promise<Attributes2Entity<A> | undefined> => {
     // TODO `defaultPagination` from master
     if (!data || isEmpty(data)) {
         return;
